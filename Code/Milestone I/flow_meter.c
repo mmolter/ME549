a13adc94a4a08c0f800c1111a045ab1ed56f1079
mmolter <mmolter@mail.bradley.edu>
2019-02-13 10:19:04 -0600

    Started interrupt code.

--- Code/Milestone I/flow_meter.c
+++ Code/Milestone I/flow_meter.c
@@ -1,23 +1,36 @@
 #define PIN_LED     PB0
-#define PIN_SENSOR  PB1
+#define PIN_SENSOR  PD2
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
+volatile uint16_t pulses = 0;
+
+ISR(INT0_vect){
+    /* Count the pulses */
+    pulses = pulses + 1;
+}
+
+void initInterrupt(void){
+    EIMSK |= (1 << INT0);                   // Enable INT0 
+    EICRA  = (1 << ISC01) | (0 << ISC00);   // Trigger on falling edge. 
+    sei();                                  // Turn on (globally) interrupts.    
+}
+
 int main(void){
-    uint16_t last_rising_edge  = 0; 
-    uint16_t pulses_per_second = 0;
-    
     /* Setup data direction registers */
-    DDRB = (1 << PIN_LED) | (0 << PIN_SENSOR);
+    DDRB = (1 << PIN_LED);
+    DDRD = (0 << PIN_SENSOR);
 
     /* Set INPUT to INPUT_PULLUP */
-    PORTB = (0 << PIN_LED) | (0 << PIN_SENSOR);
+    PORTB = (0 << PIN_LED);
+    PORTD = (0 << PIN_SENSOR);
 
     /* Main program loop. */
     while(1){
         /* IF PIN_SENSOR is LOW */
-        if (PINB & (1 << PIN_SENSOR)){
+        if (PIND & (1 << PIN_SENSOR)){
             /* Turn on LED */
             PORTB |= (1 << PIN_LED);
         }
